8f22b47eefd5c7f2d40315871055b183590507c2
Jonathan Blades <jonoblades@gmail.com>
2024-12-04 09:11:27 +0000

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import { loadEnv } from "vite";
 import node from '@astrojs/node';
 import sidecar from 'astro-sidecar';
 
-const { PUBLIC_PORT } = loadEnv(process.env.NODE_ENV || '', process.cwd(), '') || 80;
+const { PUBLIC_PORT, PUBLIC_BASE_URL } = loadEnv(process.env.NODE_ENV || '', process.cwd(), '');
 console.log(`Public port: ${PUBLIC_PORT}`);
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-
+  site: PUBLIC_BASE_URL,
   adapter: node({
     mode: 'standalone'
   }),
@@ -23,6 +23,6 @@ export default defineConfig({
   ],
   server: {
     port: parseInt(PUBLIC_PORT),
-    host: '0.0.0.0'
+    host: true
   }
 });
